51aacf9f584121875fdb9fb8f5d4ab68c31f6139
涼宮結音 <68138727+yuioto@users.noreply.github.com>
2022-07-23 15:35:34 +0800

    Update entrypoint.sh
    Signed-off-by: 涼宮結音 <68138727+yuioto@users.noreply.github.com>

--- create-db-and-upload-action/entrypoint.sh
+++ create-db-and-upload-action/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 init_path=$PWD
 mkdir upload_packages
-cp $local_path/*/*/*.tar.zst ./upload_packages/
+\cp -f $local_path/*/*/*.tar.zst ./upload_packages/
 
 if [ ! -f ~/.config/rclone/rclone.conf ]; then
     mkdir --parents ~/.config/rclone
